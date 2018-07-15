e337f3377ad2c347e513e122422cd6661ef0ed96
IzikArruda <izikarruda@gmail.com>
2018-07-15 17:20:53 -0300

    Added a WindowController and LineDrawer to properly render the game
    A WindowController will control any changes to the window and it's resolution. This will ensure the game is properly drawn to the window. It allows the game to render in 3 versions: True, Stretch and TrueRatioStretch. This script will control their activation, but the lineDrawer controls their effects.
    
    The lineDrawer will be used to render the game and it's components. Currently it will only render it's Line objects using Debug.DrawLine. It does render the lines in different sizes depending on it's resolution mode, whether the lines are made to fit the screen and whether they aim to keep the game area's ratio.

---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * The Line class that the 
+ */
+public class Line {
+    public Vector2 start;
+    public Vector2 end;
+}
+
+/*
+ * Sends requests directly to the graphics class to draw meshes created to look like lines.
+ */
+public class LineDrawer : MonoBehaviour {
+    #region Variables  --------------------------------------------------------- */
+
+    /* Screen resolution values */
+    private ResolutionMode resolutionMode;
+    private float windowWidth;
+    private float windowHeight;
+
+    /* The list of lines that need to be drawn */
+    private List<Line> lines;
+
+    /* Game area sizes */
+    private float gameAreaX;
+    private float gameAreaY;
+
+    #endregion
+
+
+    #region Built-In Unity Functions ------------------------------------------------------ */
+
+    void Start () {
+        /*
+         * Initialize the lines list
+         */
+        lines = new List<Line>();
+
+        /* Add lines in a square. For now, we assume the "game area" is a 100x100 square, with the bottom left corner being the origin */
+        gameAreaX = 100;
+        gameAreaY = 100;
+        Line newLine1 = new Line();
+        newLine1.start = new Vector2(100, 100);
+        newLine1.end = new Vector2(0, 100);
+        lines.Add(newLine1);
+        Line newLine2 = new Line();
+        newLine2.start = new Vector2(0, 100);
+        newLine2.end = new Vector2(0, 0);
+        lines.Add(newLine2);
+        Line newLine3 = new Line();
+        newLine3.start = new Vector2(0, 0);
+        newLine3.end = new Vector2(100, 0);
+        lines.Add(newLine3);
+        Line newLine4 = new Line();
+        newLine4.start = new Vector2(100, 0);
+        newLine4.end = new Vector2(100, 100);
+        lines.Add(newLine4);
+    }
+	
+	void Update () {
+        /*
+         * Draw each line in the lines list onto the screen using the DrawLine function.
+         * Depending on the current resolution mode, alter the lines before rendering them
+         */
+        Line line;
+        
+        for(int i = 0; i < lines.Count; i++) {
+            line = lines[i];
+
+            
+            DrawLine(line);
+        }
+	}
+
+    #endregion
+    
+
+    #region Set/Get Functions ------------------------------------------------------------- */
+
+    public void SetResolutionMode(ResolutionMode newMode) {
+        /*
+         * Set the current resolution mode used by the renderer
+         */
+        resolutionMode = newMode;
+    }
+
+    public void SetWindowResolution(float height, float width) {
+        /*
+         * Set the saved window resolution to the given sizes
+         */
+        windowHeight = height;
+        windowWidth = width;
+    }
+
+    #endregion
+
+
+    #region Drawing Functions ------------------------------------------------------------- */
+
+    private void DrawLine(Line line) {
+        /*
+         * Draw the given line onto the screen using the graphics class.
+         * Alter the line's size to adjust relative to the window's resolution.
+         */
+        Vector2 LStart = line.start;
+        Vector2 LEnd = line.end;
+        Vector2 centerOffset = new Vector2(gameAreaX/2f, gameAreaY/2f);
+
+        /* Don't adjust the size of the line, just set the offset to center it */
+        if(resolutionMode == ResolutionMode.True) {
+            centerOffset = new Vector2(gameAreaX/2f, gameAreaY/2f);
+        }
+
+        /* The resolution mode calls for the line to change it's size relative to the screen */
+        if(resolutionMode == ResolutionMode.Stretch) {
+            float heightRatio = windowHeight/gameAreaY;
+            float widthRatio = windowWidth/gameAreaX;
+            LStart = new Vector2(LStart.x*widthRatio, LStart.y*heightRatio);
+            LEnd = new Vector2(LEnd.x*widthRatio, LEnd.y*heightRatio);     
+            centerOffset = new Vector2(widthRatio*gameAreaX/2f, heightRatio*gameAreaY/2f);
+        }
+
+        /* Stretch to fit the screen while keeping the ratio intact */
+        else if(resolutionMode == ResolutionMode.TrueRatioStretch) {
+            float ratio = Mathf.Min(windowHeight/gameAreaY, windowWidth/gameAreaX);
+            LStart = new Vector2(LStart.x*ratio, LStart.y*ratio);
+            LEnd = new Vector2(LEnd.x*ratio, LEnd.y*ratio);     
+            centerOffset = new Vector2(ratio*gameAreaX/2f, ratio*gameAreaY/2f);
+        }
+
+
+        /* Re-position the line so that it's centered in the camera */
+        LStart -= centerOffset;
+        LEnd -= centerOffset;
+        
+
+        //Note: for now, keep it simple and simply use debug.drawLine call
+        Debug.DrawLine(LStart, LEnd);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/WindowController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* 
+ * How the game is rendered relative to the window size.
+ * 
+ * True: The window's resolution does not effect how the game is displayed
+ * Stretch: The game is stretched to ensure it covers the entire window's view
+ * TrueRatioStretch: Retain the ratio of the game, but stretch for it to fit the window
+ */
+public enum ResolutionMode {
+    NULL,
+    True,
+    Stretch,
+    TrueRatioStretch
+};
+
+/*
+ * Handle how the game reacts to window size changes
+ */
+public class WindowController : MonoBehaviour {
+    #region Variables  --------------------------------------------------------- */
+
+    #region Linked Scripts
+    public LineDrawer lineDrawer;
+    #endregion
+
+    /* Main camera */
+    public Camera mainCamera;
+
+    /* Resolution variables */
+    private ResolutionMode currentResolutionMode = ResolutionMode.NULL;
+    private float windowWidth = -1;
+    private float windowHeight = -1;
+
+    #endregion
+
+
+    #region Built-In Unity Functions  --------------------------------------------------------- */
+
+    void Start () {
+
+        /* Start the game in the true rendering mode */
+        ChangeCurrentWindowResolutionMode(ResolutionMode.TrueRatioStretch);
+    }
+
+    void Update() {
+
+        /* Catch when the window's resolution does not match the script's saved resolution */
+        if(windowWidth != Screen.width || windowHeight != Screen.height) {
+            CaughtWindowResolutionChange();
+        }
+    }
+
+    #endregion
+    
+
+    #region Resolution Functions ------------------------------------------------------- */
+
+    private void ChangeCurrentWindowResolutionMode(ResolutionMode newMode) {
+        /*
+         * Change the current resolution mode to the given newMode. If we succesfully change 
+         * the mode, inform the lineRenderer that the rendering mode has changed.
+         */
+         
+        if(newMode != currentResolutionMode && newMode != ResolutionMode.NULL) {
+
+            /* Change the rendering mode in the windowController and line renderer */
+            currentResolutionMode = newMode;
+            lineDrawer.SetResolutionMode(newMode);
+        }
+    }
+    
+    private void CaughtWindowResolutionChange() {
+        /*
+         * Called when the window's resolution has been changed.
+         * Update the script's saved window sizes to reflect the new size
+         */
+
+        windowHeight = Screen.height;
+        windowWidth = Screen.width;
+        mainCamera.orthographicSize = windowHeight/2f;
+        lineDrawer.SetWindowResolution(windowHeight, windowWidth);
+    }
+
+    #endregion
+}
